29cf183c3ec6548c0a98fa9fff919676e0a50025
Yannik Seitz <yannikseitz@outlook.de>
2020-10-06 08:51:59 +0200

    new migrations

---
+++ database/migrations/2020_10_06_064122_create_media_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('index');
+            $table->string('type');
+            $table->integer('episodes')->nullable();
+            $table->string('genre')->nullable();
+            $table->string('author')->nullable();
+            $table->string('status')->nullable();
+            $table->integer('rating')->nullable();
+            $table->string('releaseDate')->nullable();
+            $table->string('startDate')->nullable();
+            $table->string('completeDate')->nullable();
+            $table->string('lastDate')->nullable();
+            $table->unsignedBigInteger('collection_id');
+            $table->timestamps();
+
+            $table->index('collection_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media');
+    }
+}
