0f3c00bb649a836e3d17248eb51899710413432e
Yannik Seitz <yannikseitz@gmail.com>
2020-10-26 14:49:20 +0100

    added VoerroTagsInput

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,6 +20,8 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('index-table', require('./components/IndexTable.vue').default);
+Vue.component('tag-input', require('./components/TagInput.vue').default);
+Vue.component('tags-input', VoerroTagsInput);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
