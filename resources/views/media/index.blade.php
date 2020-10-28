b5b773a042fe8ceb8388cb67e084cc1075887bba
Yannik Seitz <yannikseitz@gmail.com>
2020-10-28 08:54:55 +0100

    tidied up index

--- resources/views/media/index.blade.php
+++ resources/views/media/index.blade.php
@@ -2,22 +2,13 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-3 p-5">
-            <img class="rounded-circle w-100" src="/svg/logo.png" alt="">
+    <div class="row pb-3">
+        <div class="d-flex">
+            <div class="pr-5"><strong>{{ sizeof($media) }}</strong> entries</div>
+            <div class="pr-5"><strong>999</strong> completed</div>
+            <div class="pr-5"><strong>999</strong>%</div>
         </div>
-        <div class="col-9 pt-5">
-            <div>
-            <h1>{{ $user->name }}</h1>
-            </div>
-            <div class="d-flex">
-                <div class="pr-5"><strong>{{ sizeof($media) }}</strong> entries</div>
-                <div class="pr-5"><strong>153</strong> completed</div>
-                <div class="pr-5"><strong>40</strong>%</div>
-            </div>
-            <div class="pt-4 font-weight-bold">ghghfghg</div>
-            <a href="/m/create">Medieneintrag hinzufügen</a>
-        </div>
+        <a href="/m/create">Medieneintrag hinzufügen</a>
     </div>    
     <div class="row">
         <index-table :media="{{ $media }}"></index-table>
